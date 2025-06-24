9ecad6bb803787307234793c80028fe95af2ca1a
Mega Bagas <101911572+MegaBagaTirtaKusuma@users.noreply.github.com>
2025-06-24 15:38:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -73,15 +73,6 @@ def load_custom_model(h5_path):
     return model
 
 
-# =====================
-# LOAD MODEL SEKALI (CACHE)
-# =====================
-@st.cache_resource
-def get_model():
-    return load_custom_model(download_model())
-
-model = get_model()
-
 
 
 # =====================
