72d0cec9b7a8d3a4e752a9e20f6ab58d95611bda
Mega Bagas <101911572+MegaBagaTirtaKusuma@users.noreply.github.com>
2025-06-24 15:38:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ import json
 MODEL_DIR = "model"
 MODEL_FILE = "model_resnet152.h5"
 MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
-MODEL_URL = "https://huggingface.co/bagastk/deteksi-oscc/blob/main/model_resnet152_bs8.keras"
+MODEL_URL = "https://huggingface.co/bagastk/deteksi-oscc/resolve/main/model_resnet152_bs8.keras"
+
 
 # =====================
 # 3. UNDUH MODEL
@@ -70,6 +71,17 @@ def load_custom_model(h5_path):
 
     model.load_weights(h5_path)
     return model
+
+
+# =====================
+# LOAD MODEL SEKALI (CACHE)
+# =====================
+@st.cache_resource
+def get_model():
+    return load_custom_model(download_model())
+
+model = get_model()
+
 
 
 # =====================
